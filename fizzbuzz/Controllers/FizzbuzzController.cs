f80557140ffe3f04d5bfcf3555fcd5bbe7c52e24
Jan Bernat Roselló Sbert <crimsonhold@gmail.com>
2019-06-22 13:48:51 +0200

    Fizz buzz controller

---
+++ fizzbuzz/Controllers/FizzbuzzController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace fizzbuzz.Controllers
+{
+    [Route("api/fizzbuzz")]
+    [ApiController]
+    public class FizzbuzzController : ControllerBase
+    {
+        // GET: api/Fizzbuzz
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET: api/Fizzbuzz/5
+        [HttpGet("{id}", Name = "Get")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST: api/Fizzbuzz
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT: api/Fizzbuzz/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE: api/ApiWithActions/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
